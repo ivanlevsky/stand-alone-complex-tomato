6f06aa0e5d51ccec101b0cd07ba7485f1c9c5c93
ivanlevsky <ivanlevsky@gmail.com>
2020-11-04 17:05:28 +0800

    port python dataset utils read excel function

--- src/com/databases/DatasetsTest.java
+++ src/com/databases/DatasetsTest.java
@@ -1,7 +1,6 @@
 package com.databases;
 
 import com.java.common.GlobalParams;
-import com.databases.DatasetsUtils;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,16 +9,23 @@ public class DatasetsTest {
         HashMap<String, String> ec = new HashMap<>();
         ec.put("id","int");
         ec.put("name", "str");
-        ec.put("chnname", "int");
+        ec.put("chnname", "str");
         ec.put("cast", "str");
-        ec.put("year", "int");
+        ec.put("year", "str");
         ec.put("region", "str");
         ec.put("type", "str");
         ec.put("viewed", "str");
         ec.put("want_to_review", "str");
-
-        DatasetsUtils.readExcel(GlobalParams.excelDatasets, "movie", true, ec);
-//        readExcel(GlobalParams.excelDatasets, "movie", false);
+        ArrayList<String> data;
+        data = DatasetsUtils.readExcel(GlobalParams.excelDatasets, "movie", true, ec);
+        for (String s: data) {
+            System.out.println(s);
+        }
+        ec.put("year","int");
+        data = DatasetsUtils.readExcel(GlobalParams.excelDatasets, "movie", false, ec);
+        for (String s: data) {
+            System.out.println(s);
+        }
     }
 
 }
--- src/com/databases/DatasetsUtils.java
+++ src/com/databases/DatasetsUtils.java
@@ -5,12 +5,15 @@ import org.apache.poi.ss.usermodel.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DatasetsUtils {
-    public static void readExcel(String excelFile, String sheetName, boolean removeHeader
+    public static ArrayList<String> readExcel(String excelFile, String sheetName, boolean removeHeader
             , HashMap<String,String> excelConverter
     ){
+        String splitText = "S_P_L_I_T";
+        ArrayList<String> result = new ArrayList<>();
         try {
             InputStream inp = new FileInputStream(excelFile);
             Workbook wb = WorkbookFactory.create(inp);
@@ -19,31 +22,34 @@ public class DatasetsUtils {
             int startRow = removeHeader?1:0;
             int cols = sheet.getRow(0).getLastCellNum();
             HashMap<Integer, String> colType = new HashMap<>();
-            if(excelConverter.size() > 0){
+            if(excelConverter != null){
                 for (int i = 0; i < cols; i++) {
                     colType.put(i,
                             excelConverter.get(sheet.getRow(0).getCell(i).toString()));
                 }
             }
-            excelConverter.clear();
             StringBuilder temp;
             DataFormatter dataFormatter = new DataFormatter();
 
             for (int i = startRow; i <= rows; i++) {
                 temp = new StringBuilder();
                 for (int j = 0; j < cols; j++) {
-                    if(colType.get(j).equals("str")){
-                        temp.append(dataFormatter.formatCellValue(sheet.getRow(i).getCell(j))).append(";");
+                    if(colType.size()>0) {
+                        if (colType.get(j).equals("str")) {
+                            temp.append(dataFormatter.formatCellValue(sheet.getRow(i).getCell(j))).append(splitText);
+                        } else {
+                            temp.append(sheet.getRow(i).getCell(j)).append(splitText);
+                        }
                     }else {
-                        temp.append(sheet.getRow(i).getCell(j)).append(";");
+                        temp.append(dataFormatter.formatCellValue(sheet.getRow(i).getCell(j))).append(splitText);
                     }
                 }
-                System.out.println(temp);
+                result.add(temp.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
+        return result;
     }
 }
