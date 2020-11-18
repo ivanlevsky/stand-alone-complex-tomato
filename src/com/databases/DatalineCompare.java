c60c58c0d45a2038a8381ffcb9f256af7ed37914
ivanlevsky <ivanlevsky@gmail.com>
2020-11-18 14:45:46 +0800

    compare two string arraylist read from text, func1 is faster than func2

---
+++ src/com/databases/DatalineCompare.java
@@ -0,0 +1,58 @@
+package com.databases;
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class DatalineCompare {
+    public static void main(String[] args) throws IOException {
+        File tf1 = new File("C:\\Users\\ivanovsky\\Desktop\\nds1.txt");
+        File tf2 = new File("C:\\Users\\ivanovsky\\Desktop\\nds2.txt");
+        ArrayList<String> text1 = (ArrayList<String>) Files.readLines(tf1, Charset.forName("GBK"));
+        ArrayList<String> text2 = (ArrayList<String>) Files.readLines(tf2, Charset.forName("GBK"));
+
+        diffFunc2(text1, text2);
+        diffFunc1(text1, text2);
+    }
+    private static void diffFunc2(ArrayList<String> list1, ArrayList<String> list2){
+        Long startTime = System.currentTimeMillis();
+        ArrayList<String> error = new ArrayList<>();
+        for (int i = 0; i < list2.size(); i++) {
+            for (int j = 0; j < list1.size(); j++) {
+                if (list1.get(j).equals(list2.get(i))) {
+                    break;
+                }else {
+                    if((j == list2.size() -1)){
+                        error.add("line: " + i + " , " + list2.get(i));
+                    }
+                }
+            }
+        }
+        for (String s : error) {
+            System.out.println(s);
+        }
+        Long endTime = System.currentTimeMillis();
+        System.out.println("func2 calculate time:" + (endTime - startTime));
+    }
+
+    private static void diffFunc1(ArrayList<String> list1, ArrayList<String> list2) {
+        Long startTime = System.currentTimeMillis();
+        ArrayList<String> error = (ArrayList<String>) list2.clone();
+        for (int i = list1.size() - 1; i >= 0; i--) {
+            for (int j = list2.size() - 1; j >= 0; j--) {
+                if (list2.get(j).equals(list1.get(i))) {
+                    list2.remove(j);
+                    list1.remove(i);
+                    break;
+                }
+            }
+        }
+        for (String s : list2) {
+            System.out.println("line: " + error.indexOf(s) + " , " + s);
+        }
+        Long endTime = System.currentTimeMillis();
+        System.out.println("func1 calculate time:" + (endTime - startTime));
+    }
+}
