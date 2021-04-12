010223999c450e7413497bd5649be8ee01cde849
ivanlevsky <ivanlevsky@gmail.com>
2021-04-12 15:59:39 +0800

    add StringUtilsTest

---
+++ src/com/junit/test/StringUtilsTest.java
@@ -0,0 +1,30 @@
+package com.junit.test;
+
+import com.java.common.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest extends StringUtils {
+    private String textToSplit1;
+    private String textToSplit2;
+    private String[] textGroup1;
+    private String[] textGroup2;
+
+    @BeforeEach
+    void setUp() {
+        textToSplit1 = "abe123*(*#*123山大佛i哦123SOEO";
+        textToSplit2 = "124{abiid|DKDL都是}444{123 |**@)}";
+        textGroup1 = new String[]{"abe", "*(*#*", "山大佛i哦", "SOEO"};
+        textGroup2 = new String[]{"124", "abiid|DKDL都是", "444", "123 |**@)"};
+    }
+
+    @Test
+    void testSplitStringByRegex() {
+        assertAll("textSplit",
+                () -> assertArrayEquals(textGroup1, splitStringByRegex("123",textToSplit1)),
+                () -> assertArrayEquals(textGroup2, splitStringByRegex("\\{|\\}",textToSplit2))
+        );
+    }
+}
