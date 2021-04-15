cfaf21be491e533ee296214f34fee3acddbded3f
ivanlevsky <ivanlevsky@gmail.com>
2021-04-15 14:40:12 +0800

    add junit suite test

---
+++ src/com/junit/test/AllJunitTests.java
@@ -0,0 +1,19 @@
+package com.junit.test;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+/***
+ * //select classes: @SelectClasses({StringUtilsTest.class, CryptoUtilsTest.class})
+ *  annotation equals below
+ * //select package: @SelectPackages("com.junit.test")
+ * //exclude class endwith "WindowsOsUtilsTest": @ExcludeClassNamePatterns({"^.*WindowsOsUtilsTest?$"})
+ */
+@RunWith(JUnitPlatform.class)
+@SuiteDisplayName("JUnit Platform Suite Demo")
+@SelectPackages("com.junit.test")
+public class AllJunitTests {
+
+}
