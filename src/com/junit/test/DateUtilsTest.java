1845e76441fc793b09d172a47f352d5348af0138
ivanlevsky <ivanlevsky@gmail.com>
2021-08-31 16:51:12 +0800

    add getLastDayOfMonth, getFirstDayOfMonth tests

--- src/com/junit/test/DateUtilsTest.java
+++ src/com/junit/test/DateUtilsTest.java
@@ -12,13 +12,16 @@ class DateUtilsTest extends DateUtils{
 	private String oldDate;
     private int calcDate1;
     private int calcDate2;
-
+    private String firstDayOfMonth;
+    private String lastDayOfMonth;
 
     @BeforeEach
     void setUp() {
     	oldDate = "2021-08-25";
     	calcDate1 = 10;
     	calcDate2 = -10;
+    	firstDayOfMonth = "2020-02-25";
+    	lastDayOfMonth = "2021-08-01";
     }
 
     @Test
@@ -27,7 +30,9 @@ class DateUtilsTest extends DateUtils{
                 () -> assertEquals("2021-09-04", CalculateDate(oldDate, calcDate1, false)),
                 () -> assertEquals("2021-09-08", CalculateDate(oldDate, calcDate1, true)),
                 () -> assertEquals("2021-08-15", CalculateDate(oldDate, calcDate2, false)),
-                () -> assertEquals("2021-08-11", CalculateDate(oldDate, calcDate2, true))
+                () -> assertEquals("2021-08-11", CalculateDate(oldDate, calcDate2, true)),
+                () -> assertEquals("2020-02-01", getFirstDayOfMonth(firstDayOfMonth)),
+                () -> assertEquals("2021-08-31", getLastDayOfMonth(lastDayOfMonth))
         );
     }
 
