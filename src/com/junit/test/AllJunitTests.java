c1e78674581731adfa15194864fbdaa66fd8c6ba
ivanlevsky <ivanlevsky@gmail.com>
2023-04-24 14:33:17 +0800

    Update AllJunitTests.java

--- src/com/junit/test/AllJunitTests.java
+++ src/com/junit/test/AllJunitTests.java
@@ -3,7 +3,7 @@ package com.junit.test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.SuiteDisplayName;
-// import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;
 import org.junit.platform.suite.api.Suite;
 
 /***
@@ -12,8 +12,8 @@ import org.junit.platform.suite.api.Suite;
  * //select package: @SelectPackages("com.junit.test")
  * //exclude class endwith "WindowsOsUtilsTest": @ExcludeClassNamePatterns({"^.*WindowsOsUtilsTest?$"})
  */
-// @RunWith(JUnitPlatform.class)  -> junit4
-@Suite
+@RunWith(JUnitPlatform.class)  
+// @Suite
 @SuiteDisplayName("JUnit Platform Suite Demo")
 @SelectPackages("com.junit.test")
 public class AllJunitTests {
